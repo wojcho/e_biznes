c49884b107a4b1d8d865bf3ac4a7630bc36e43b4
wojcho <wojhoo3@gmail.com>
2026-06-27 12:42:50 +0200

    Adjust prices to be more varied

--- 3_ktor/src/main/kotlin/Shop.kt
+++ 3_ktor/src/main/kotlin/Shop.kt
@@ -23,25 +23,27 @@ val categories = arrayOf(
 )
 
 val products = arrayOf(
-  Product(1, "Zielona Herbata", 100, 3),
-  Product(2, "Biała Herbata", 100, 3),
-  Product(3, "Żółta Herbata", 100, 3),
-  Product(4, "Czarna Herbata", 100, 3),
-  Product(5, "Oolong", 100, 3),
-  Product(6, "Earl Gray", 100, 3),
-  Product(7, "Kawa Inka", 100, 2),
-  Product(8, "Rooibos", 100, 2),
-  Product(9, "Yerba Mate", 100, 2),
-  Product(10, "Wywar z Mięty", 100, 2),
-  Product(11, "Wywar z Pokrzywy", 100, 2),
-  Product(12, "Wywar z Szałwi", 100, 2),
-  Product(13, "Kurkuma", 100, 1),
-  Product(14, "Szafran", 100, 1),
-  Product(15, "Koper", 100, 1),
-  Product(16, "Gorczyca", 100, 1),
-  Product(17, "Wanilia", 100, 1),
-  Product(18, "Imbir", 100, 1),
-  Product(19, "Cynamon", 100, 1),
+  Product(1, "Zielona Herbata", 65, 3),
+  Product(2, "Biała Herbata", 140, 3),
+  Product(3, "Żółta Herbata", 180, 3),
+  Product(4, "Czarna Herbata", 45, 3),
+  Product(5, "Oolong", 160, 3),
+  Product(6, "Earl Grey", 55, 3),
+
+  Product(7, "Kawa Inka", 85, 2),
+  Product(8, "Rooibos", 95, 2),
+  Product(9, "Yerba Mate", 75, 2),
+  Product(10, "Wywar z Mięty", 35, 2),
+  Product(11, "Wywar z Pokrzywy", 30, 2),
+  Product(12, "Wywar z Szałwi", 45, 2),
+
+  Product(13, "Kurkuma", 55, 1),
+  Product(14, "Szafran", 12000, 1),
+  Product(15, "Koper", 25, 1),
+  Product(16, "Gorczyca", 18, 1),
+  Product(17, "Wanilia", 1500, 1),
+  Product(18, "Imbir", 60, 1),
+  Product(19, "Cynamon", 70, 1),
 )
 
 fun productsOfCategory(products: Array<Product>, categoryId: Int): List<Product> {
